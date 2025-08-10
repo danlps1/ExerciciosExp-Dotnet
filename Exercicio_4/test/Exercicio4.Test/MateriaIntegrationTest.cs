ceee1d3950013d00fd7f3618e360b67130b39592
Daniel <danieloliv.contato@gmail.com>
2025-08-10 18:46:10 -0300

    feat: Testes GET Materias

--- Exercicio_4/test/Exercicio4.Test/MateriaIntegrationTest.cs
+++ Exercicio_4/test/Exercicio4.Test/MateriaIntegrationTest.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Json;
 using System.Text.Json;
 using Exercicio4.Application.Dtos;
+using Exercicio4.Domain.Entities;
 using Exercicio4.Test.Factory;
 using FluentAssertions;
 
@@ -16,21 +17,52 @@ public class MateriaIntegrationTest : IClassFixture<Exercicio4WebApplicationFact
         _httpClient = factory.CreateClient();
     }
 
+    private async Task CriarMateriaAsync(string nome = "Teste")
+    {
+        var materia = new MateriaDto { Nome = nome };
+        await _httpClient.PostAsJsonAsync("/api/Materia", materia);
+    }
+
     [Fact]
     public async Task Deve_Cadastrar_Materia_Sucesso()
     {
-        // Arrange
         var materia = new MateriaDto { Nome = "Test" };
 
-        // Act
         var response = await _httpClient.PostAsJsonAsync("/api/Materia", materia);
 
-        // Assert
         response.StatusCode.Should().Be(HttpStatusCode.Created);
 
-        var responseData = await response.Content.ReadFromJsonAsync<MateriaDto>();
+        var responseData = await response.Content.ReadFromJsonAsync<Materia>();
         responseData.Should().NotBeNull();
-        responseData!.Nome.Should().Be("Test");
+        responseData.Nome.Should().Be("Test");
     }
 
+    [Fact]
+    public async Task Deve_Listar_Materias_Sucesso()
+    {
+        await CriarMateriaAsync();
+
+        var response = await _httpClient.GetAsync("/api/Materia");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseData = await response.Content.ReadFromJsonAsync<List<Materia>>();
+        responseData.Should().NotBeNull();
+        responseData.Should().ContainSingle("Teste");
+    }
+
+    [Fact]
+    public async Task Deve_Buscar_Materia_Por_Id_Sucesso()
+    {
+        await CriarMateriaAsync();
+
+        var response = await _httpClient.GetAsync($"/api/Materia/1");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseData = await response.Content.ReadFromJsonAsync<Materia>();
+        responseData.Should().NotBeNull();
+        responseData.Id.Should().Be(1);
+        responseData.Nome.Should().Be("Teste");
+    }
 }
