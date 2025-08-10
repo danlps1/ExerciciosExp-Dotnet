300bfc953d0e74c7f37b2330cb08140d4b8be3dd
Daniel <danieloliv.contato@gmail.com>
2025-08-10 14:41:07 -0300

    feat: CadastrarMateriaSucesso test

---
+++ Exercicio_4/test/Exercicio4.Test/MateriaIntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Exercicio4.Application.Dtos;
+using Exercicio4.Test.Factory;
+using FluentAssertions;
+
+namespace Exercicio4.Test;
+
+public class MateriaIntegrationTest : IClassFixture<Exercicio4WebApplicationFactory>
+{
+    private readonly HttpClient _httpClient;
+
+    public MateriaIntegrationTest(Exercicio4WebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Deve_Cadastrar_Materia_Sucesso()
+    {
+        // Arrange
+        var materia = new MateriaDto { Nome = "Test" };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync("/api/Materia", materia);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var responseData = await response.Content.ReadFromJsonAsync<MateriaDto>();
+        responseData.Should().NotBeNull();
+        responseData!.Nome.Should().Be("Test");
+    }
+
+}
